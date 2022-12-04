be5e575c1587c93894ff025092bab654bfaf918c
Michail Kulaga <michailkulaga@gmail.com>
2022-12-04 16:37:31 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -28,3 +28,36 @@ for game in games :
     
 
 print(resultScore)
+
+
+
+# Part 2
+
+def mapHandToNumber(hand):
+    if hand == "A" : return 0
+    if hand == "B" : return 1
+    if hand == "C" : return 2
+
+def gameResult(hand2):
+    if hand2 == "X" : return 0
+    if hand2 == "Y" : return 3
+    if hand2 == "Z" : return 6
+
+def secondHandShouldBe(hand1, gameResult):
+    if gameResult == 3 : return hand1
+    elif gameResult == 0 : return (hand1+2)%3
+    else : return (hand1+1)%3
+
+inputArray = open("inputDay2.txt").read()
+
+games = inputArray.split("\n")
+resultScore = 0
+for game in games :
+    hands = game.split(" ")
+    hand1 = mapHandToNumber(hands[0])
+    gameResultParam = gameResult(hands[1])
+    # game result
+    resultScore += gameResultParam
+    resultScore += secondHandShouldBe(hand1,gameResultParam) + 1
+
+print(resultScore)
