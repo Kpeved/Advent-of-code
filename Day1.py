1b79b4be965f7889a4a742181e390d069aa1c3bf
Michail Kulaga <michailkulaga@gmail.com>
2022-12-04 02:19:33 +0000

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -9,3 +9,16 @@ for caloryGroup in caloryGroups :
     maxResult = max(tempSum, maxResult)
 
 print("MaxResult" + str(maxResult))
+
+# Part 2
+
+calories = input("Enter calories array")
+
+caloryGroups = calories.split("  ")
+sortedResult = []
+for caloryGroup in caloryGroups :
+    sortedResult.append(sum(map(int,caloryGroup.split(" "))))
+
+sortedResult.sort(reverse=True)
+print("Sum of max 3 :" + str(sortedResult[0] + sortedResult[1] + sortedResult[2]))
+
