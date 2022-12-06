87a609c4eadd3215e3b048a51bd62e09c37bef10
Michail Kulaga <michailkulaga@gmail.com>
2022-12-06 22:26:57 +0000

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -19,3 +19,26 @@ for pair in pairs:
 
 print(counter)
 
+# Part 2
+
+# Day4
+inputArray = open("InputDay4.txt").read()
+
+pairs = inputArray.split("\n")
+counter = 0
+for pair in pairs:
+    print(pair)
+    (range1, range2) = pair.split(",")
+    
+    (start1, end1) = range1.split("-")
+    (start2, end2) = range2.split("-")
+    start1 = int(start1)
+    start2 = int(start2)
+    end1 = int(end1)
+    end2 = int(end2)
+    if start1 > end2 or start2 > end1 :
+        continue
+    counter +=1
+        
+
+print(counter)
