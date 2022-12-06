1b092e3790e24174547bbdf0f4f916fd78bc7b8f
Michail Kulaga <michailkulaga@gmail.com>
2022-12-06 00:27:50 +0000

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -28,3 +28,55 @@ for rucksack in rucksacks:
         index += 1
         
 print(str(resultSum))
+
+# Part 2
+
+aIndex = ord('a')
+zIndex = ord('z')
+AIndex = ord('A')
+ZIndex = ord('Z')
+
+def group_by(array, n):
+    finalArray = []
+    
+    smallArray = []
+    index = 0
+    for item in array:
+        if index % n == 0 and index != 0 :
+            finalArray.append(smallArray)
+            smallArray = []
+        index += 1
+        smallArray.append(item)
+        
+    finalArray.append(smallArray)
+    return finalArray
+
+inputArray = open("InputDay3.txt").read()
+rucksacks = group_by(inputArray.split("\n"), 3)
+print(rucksacks)
+
+resultSum = 0
+for rucksackGroup in rucksacks:
+    
+    lettersArray = [0]*53
+    power = 0
+    for rucksack in rucksackGroup:
+        for ch in rucksack:
+            charAsciiIndex = ord(ch)
+            if charAsciiIndex >= aIndex and charAsciiIndex <= zIndex: 
+                chIndex = charAsciiIndex - aIndex + 1
+            elif charAsciiIndex >= AIndex and charAsciiIndex <= ZIndex: 
+                chIndex = charAsciiIndex - AIndex + 27
+            print(ch + " -> " + str(chIndex))
+            lettersArray[chIndex] = lettersArray[chIndex] | pow(2, power)
+            print(lettersArray) 
+        power +=1
+        
+        j = 0
+        for letter in lettersArray:
+            if letter == 7 :
+                resultSum += j
+                break
+            j += 1
+    
+print(str(resultSum))
